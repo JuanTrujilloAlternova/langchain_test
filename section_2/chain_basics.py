58079e16fab44ec3e7dbfcf9121e27b72097492f
Juan David Trujillo <juan.trujillo@alternova.com>
2024-05-25 17:07:53 -0500

    modify: join chains

--- section_2/chain_basics.py
+++ section_2/chain_basics.py
@@ -1,4 +1,4 @@
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
@@ -12,17 +12,36 @@ code_prompt = PromptTemplate(
 
 code_chain_part_1 = LLMChain(
     llm=llm,
-    prompt=code_prompt
+    prompt=code_prompt,
+    output_key="code"
 )
 
 task = input("Enter a code task you want to generate code for: ")
 language = input("Enter the language you want to generate code for: ")
 
-result = code_chain_part_1(
-    {
-        "language": language,
-        "task": task
-    }
+code_prompt_2 = PromptTemplate(
+    template="Write a the code to test for the following {language} code:\n{code}",
+    input_variables=["language", "code"],
 )
 
-print(result)
+code_chain_part_2 = LLMChain(
+    llm=llm,
+    prompt=code_prompt_2,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain_part_1, code_chain_part_2],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = chain({
+    "language": language,
+    "task": task
+})
+
+print("Generated code and test:")
+print(result["code"])
+print(result["test"])
+
